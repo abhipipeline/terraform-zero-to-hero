4f693a332c72a3ca6229c772821ac69e123de1d7
abhipipeline <b21cs042@kitsw.ac.in>
2026-09-19 17:28:26 +0530

    Update provider.tf

--- Day-2/PROJECT-vpc-with-ec2/provider.tf
+++ Day-2/PROJECT-vpc-with-ec2/provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.11.0"
+    google = {
+      source  = "hashicorp/google"
+      version = "~>5.11"
     }
   }
 }
 
-provider "aws" {
+provider "google" {
   # Configuration options
-  region = "us-east-1"
+  region = "us-central1"
 }
